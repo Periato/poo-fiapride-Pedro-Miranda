dbcb0daee08a39fe369ac61c9bab26c132c4d71a
PedroMCRiato <pedro.mcriato@gmail.com>
2026-05-09 16:03:22 -0300

    feat: aula02 - adicona metodos com parametros e retorno

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -6,18 +6,35 @@ public class SistemaPrincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Animal animal1 = new Animal();
-		animal1.especie = "Leão";
-		animal1.idade = 8;
+		Animal animal1 = new Animal("Leão", "Scar", 190.0);
+		Animal animal2 = new Animal("Iguana", "Irineu", 2.0);
 		
-		Animal animal2 = new Animal();
-		animal2.especie = "Iguana";
-		animal2.idade = 9;
-	
-	
 		System.out.println("--- Sistema FiapRide ---");
-        System.out.println("Espécie: " + animal1.especie + " | Idade" + animal1.idade);
-        System.out.println("Espécie: " + animal2.especie + " | Idade" + animal2.idade);
+        System.out.println("Animal 1: " + animal1.nome + " | Espécie: " + animal1.especie + " | Peso " + animal1.peso);
+        System.out.println("Animal 2: " + animal2.nome + " | Espécie: " + animal2.especie + " | Peso " + animal2.peso);
+        
+        // ---Testes animal 1 (Scar)---
+        System.out.println("\n>>>Testando: " + animal1.nome);
+        
+        // Teste válido: Fazendo aniversário de 2 anos
+        animal1.fazerAniversario(2);
+        
+        // Teste inválido: Alimentação excessiva (25kg é > 10% de 190kg)
+        animal1.alimentar(25.0);
+        
+        // ---Testes animal 2 (Irineu)---
+        System.out.println("\n>>>Testando: " + animal2.nome);
+        
+        // Teste válido: Alimentação leve (0.1kg é 5% de 2kg)
+        animal2.alimentar(0.1);
+        
+        // Teste inválido: Idade negativa
+        animal2.fazerAniversario(-5);
+        
+        // --- STATUS FINAL ---
+     		System.out.println("\n--- Resumo Atualizado Sistema FiapRide ---");
+            System.out.println("Espécie: " + animal1.especie + " | Nome: " + animal1.nome + " | Idade: " + animal1.idade + " | Peso: " + animal1.peso + "kg");
+            System.out.println("Espécie: " + animal2.especie + " | Nome: " + animal2.nome + " | Idade: " + animal2.idade + " | Peso: " + animal2.peso + "kg");
 	}
 
 }
--- src/br/com/fiapride/model/Animal.java
+++ src/br/com/fiapride/model/Animal.java
@@ -2,6 +2,38 @@ package br.com.fiapride.model;
 
 public class Animal {
 	public String especie;
+	public String nome;
+	public double peso;
 	public int idade;
 	
+	
+	public Animal(String especie, String nome, double pesoInicial) {
+		this.especie = especie;
+		this.nome = nome;
+		this.peso = pesoInicial;
+		this.idade = 0;
+	}
+	
+	// Método 1: Envelhecer o animal
+	// Regra de negócio: O animal só pode envelhecer uma quantidade positiva de anos.
+	public void fazerAniversario(int anos) {
+		if (anos > 0) {
+			this.idade += anos;
+			System.out.println(nome + " agora tem " + idade + " anos!");
+		} else {
+			System.out.println("Erro: A quantidade de anos para envelhecer deve ser maior que zero.");
+		}
+	}
+	// Método 2: Alteração de peso
+	// Regra de negócio: O ganho de peso deve ser positivo e não pode exceder 10% do peso atual
+	public void alimentar(double quantidadeComida) {
+		if (quantidadeComida > 0 && quantidadeComida <= (this.peso * 0.10)) {
+			this.peso += quantidadeComida;
+			System.out.println(nome + " comeu " + quantidadeComida + "kg. Peso atual: " + this.peso + "kg.");
+		} else if (quantidadeComida > (this.peso * 0.10)){
+			System.out.println("A quantidade de comida é excessiva.");
+		} else {
+			System.out.println("Erro: a quantidade de comida deve ser maior que zero!");
+		}
+	}
 }
